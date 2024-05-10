14e7d0c9c5e7a40ed05630c56b58e163f9a869dd
Aaaacooooo <acoray06@gmail.com>
2024-05-10 09:23:10 +0100

    Like!

--- database/migrations/2024_05_09_120346_create_sexes_table.php
+++ database/migrations/2024_05_09_120346_create_sexes_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('sexes', function (Blueprint $table) {
             $table->id();
-            $table->string('category')->unique();
+            $table->enum('category', ['M', 'F']);
             $table->timestamps();
         });
     }
